e581d63ec8d7b19ea2fab37339281cdb8dd46155
Anderson Fae <andersonfae.owl@gmail.com>
2022-07-25 10:17:18 -0300

    Footer 50%

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+
 export function Footer() {
-  return;
+  return (
+    <>
+      <footer className="text-center text-white bg-black">
+        <div classNameName="container p-6">
+          <div className="">
+            <p className="flex justify-center items-center">
+              <div className="text-center p-4">
+                COPYRIGHT © 2022 Rocket Moon. ALL RIGHTS RESERVED.
+              </div>
+              <Link to={"/credits"}>
+                <p>✨ Credits!</p>
+              </Link>
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { Footer } from "../../components/Footer";
 
 export function Home() {
   return (
@@ -7,6 +8,7 @@ export function Home() {
       <Link to={"/signup"}>
         <button>Sign Up</button>
       </Link>
+      <Footer />
     </>
   );
 }
