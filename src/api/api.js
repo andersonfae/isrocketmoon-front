ba58c8e9f2e3dd886a55352fcbac04f38b1da06b
Anderson Fae <andersonfae.owl@gmail.com>
2022-07-28 17:21:31 -0300

    API Deploy

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:4000",
-  production: "https://api-deploy-test-iron.herokuapp.com",
+  production: "https://isrocketmoon.herokuapp.com",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
--- src/pages/Job/index.js
+++ src/pages/Job/index.js
@@ -1,7 +1,44 @@
+import { Footer } from "../../components/Footer";
+import { Ball } from "../../components/GradientBall";
+import { Navbar } from "../../components/Navbar";
+import Floating from "../../images/floatingBoard.png";
+
 export function Job() {
   return (
     <>
-      <h1>Job Page</h1>
+      <Navbar />
+      <img
+        src={Floating}
+        alt="floatingBoard"
+        className="fixed top-10 right-15 -z-10"
+      />
+      <div className="inline-flex space-x-8 items-center justify-start p-8 mt-48 mx-4">
+        <div className="inline-flex flex-col space-y-2.5 items-start justify-start flex-1">
+          <p className="w-full text-xl leading-10 text-center text-white">
+            $999K
+          </p>
+          <p className="w-full text-sm text-center text-white">
+            IN JOBS TRADED
+          </p>
+        </div>
+        <div className="inline-flex flex-col space-y-2.5 items-start justify-start flex-1">
+          <p className="w-full text-xl leading-10 text-center text-white">
+            999k
+          </p>
+          <p className="w-full text-sm text-center text-white">GAMERS</p>
+        </div>
+        <div className="inline-flex flex-col space-y-2.5 items-start justify-start flex-1">
+          <p className="w-full text-xl leading-10 text-center text-white">
+            10K
+          </p>
+          <p className="w-full text-sm text-center text-white">ACTIVE JOBS</p>
+        </div>
+      </div>
+      <p className="text-xl leading-9 text-white mx-4 mt-4 font-serif">
+        See jobs on{" "}
+      </p>
+      <Ball />
+      <Footer />
     </>
   );
 }
