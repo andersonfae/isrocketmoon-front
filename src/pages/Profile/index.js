f63af2524c7785b819e9209c1bd16c2bbbec6fb1
Anderson Fae <andersonfae.owl@gmail.com>
2022-07-28 09:35:57 -0300

    Profile Almost Done

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -107,9 +107,11 @@ export function Profile() {
         />
         <div className="flex flex-col space-y-2.5 items-start justify-start w-full pb-5 border-b border-gray-300">
           <div className="flex flex-col space-y-1 items-start justify-start">
-            <p className="absolute right-5 top-5 text-black underline text-xs">
-              Edit profile
-            </p>
+            <Link to={`/user/update-profile/${user._id}`}>
+              <p className="absolute right-5 top-5 text-black underline text-xs">
+                Edit profile
+              </p>
+            </Link>
             <p className="text-xl leading-9">{user.name}</p>
             <div className="inline-flex space-x-2.5 items-center justify-start">
               <div className="flex items-center justify-center px-2.5 py-1 bg-[#1CBDF9] rounded-full">
@@ -121,7 +123,10 @@ export function Profile() {
           <p className="opacity-60 w-full text-base">{user.description}</p>
         </div>
 
-        <p className="text-xs underline text-center text-red-700">
+        <p
+          className="text-xs underline text-center text-red-700"
+          onClick={handleDelete}
+        >
           Delete account
         </p>
       </div>
